71acc8c9ef394656afe9c7959709282d020d0de5
Thosyn Pax <thosynpax@users.noreply.github.com>
2024-12-17 18:21:31 +0100

    adjusted about section

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -8,7 +8,7 @@ const About = () => {
                     <div className="col-lg-12">
                         <SlideUp className="founder-say-part text-center">
                             <h3>At AfriHeals Careers, we are dedicated to empowering individuals through personalized one-on-one mentoring.
-                                <span>Our goal is to equip you with essential tech skills to excel in today’s fast-paced world. </span>
+                                <span> Our goal is to equip you with essential tech skills to excel in today’s fast-paced world. </span>
                                  Together, we’ll unlock your
                                   full potential and help you achieve your career goals. Let’s get started!</h3>
                             <div className="founder-name-part">
