58d8e2411a66c88ff95db22c0a70247b34428b12
Thosyn Pax <thosynpax@users.noreply.github.com>
2024-12-31 00:14:25 +0100

    added marquee

--- src/components/sections/services.tsx
+++ src/components/sections/services.tsx
@@ -2,35 +2,40 @@ import '../../assets/css/custom.css';
 
 const Marquee = () => {
     return (
+        <>
         <div className="marquee-container">
             <div className="marquee">
-            <span>JIRA <span className="star-icon">★</span></span>
-            <span>FIGMA <span className="star-icon">★</span></span>
-            <span>TESTRAIL <span className="star-icon">★</span></span>
-            <span>CONFLUENCE <span className="star-icon">★</span></span>
-            <span>TRELLO <span className="star-icon">★</span></span>
-            <span>SCRUMWISE <span className="star-icon">★</span></span>
-            <span>GITHUB <span className="star-icon">★</span></span>
-            <span>PHOTOSHOP <span className="star-icon">★</span></span>
-            <span>ADOBE XD <span className="star-icon">★</span></span>
-            <span>MIRO <span className="star-icon">★</span></span>
-            <span>NOTION <span className="star-icon">★</span></span>
-            <span>SLACK <span className="star-icon">★</span></span>
-            <span>VS CODE <span className="star-icon">★</span></span>
-            <span>WEBFLOW <span className="star-icon">★</span></span>
-            <span>POSTMAN <span className="star-icon">★</span></span>
-            <span>REST API <span className="star-icon">★</span></span>
-            <span>REACT JS <span className="star-icon">★</span></span>
-            <span>PYTHON <span className="star-icon">★</span></span>
-            <span>UI/UX DESIGN <span className="star-icon">★</span></span>
-            <span>AGILE METHODOLOGY <span className="star-icon">★</span></span>
-            <span>BUSINESS ANALYSIS <span className="star-icon">★</span></span>
-            <span>DATA ANALYSIS <span className="star-icon">★</span></span>
-            <span>PRODUCT MANAGEMENT <span className="star-icon">★</span></span>
-            <span>WEB DEVELOPMENT <span className="star-icon">★</span></span>
-            <span>SOFTWARE TESTING <span className="star-icon">★</span></span>
+                <span>JIRA <span className="star-icon">★</span></span>
+                <span>FIGMA <span className="star-icon">★</span></span>
+                <span>TESTRAIL <span className="star-icon">★</span></span>
+                <span>CONFLUENCE <span className="star-icon">★</span></span>
+                <span>TRELLO <span className="star-icon">★</span></span>
+                <span>SCRUMWISE <span className="star-icon">★</span></span>
+                <span>GITHUB <span className="star-icon">★</span></span>
+                <span>PHOTOSHOP <span className="star-icon">★</span></span>
+                <span>ADOBE XD <span className="star-icon">★</span></span>
+                <span>MIRO <span className="star-icon">★</span></span>
+                <span>NOTION <span className="star-icon">★</span></span>
+                <span>SLACK <span className="star-icon">★</span></span>
+                <span>VS CODE <span className="star-icon">★</span></span>
+                <span>WEBFLOW <span className="star-icon">★</span></span>
+                <span>POSTMAN <span className="star-icon">★</span></span>
+                <span>REST API <span className="star-icon">★</span></span>
+                <span>REACT JS <span className="star-icon">★</span></span>
+                <span>PYTHON <span className="star-icon">★</span></span>
+            </div>
+
+            <div className="marquee m-22">
+                <span>UI/UX DESIGN <span className="star-icon">★</span></span>
+                <span>AGILE METHODOLOGY <span className="star-icon">★</span></span>
+                <span>BUSINESS ANALYSIS <span className="star-icon">★</span></span>
+                <span>DATA ANALYSIS <span className="star-icon">★</span></span>
+                <span>PRODUCT MANAGEMENT <span className="star-icon">★</span></span>
+                <span>WEB DEVELOPMENT <span className="star-icon">★</span></span>
+                <span>SOFTWARE TESTING <span className="star-icon">★</span></span>
             </div>
         </div>
+        </>
 
         
     );
