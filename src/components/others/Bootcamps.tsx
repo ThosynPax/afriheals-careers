90cdf17c6b15074a48afeb4d2da7d26bfbbb36a5
Thosyn Pax <thosynpax@users.noreply.github.com>
2025-01-10 00:43:45 +0100

    modified bootcamp pae

--- src/components/others/Bootcamps.tsx
+++ src/components/others/Bootcamps.tsx
@@ -80,6 +80,16 @@ const Bootcamps = () => {
                     </SectionTitle.Description>
 
                     <SectionTitle.Description>
+                    <SectionTitle.Title>Here’s why our Bootcamp works:</SectionTitle.Title>
+                        <ul>
+                            <li>✅ Learn from experienced instructors</li>
+                            <li>✅ Work on real-world projects</li>
+                            <li>✅ Get the support you need to land your first tech role</li>
+                            <li>✅ Virtual Internship Opportunities</li>
+                        </ul>
+                    </SectionTitle.Description>
+
+                    <SectionTitle.Description>
                         <Slider {...sliderSettings}>
                             <div className="slider-item">
                                 <img 
@@ -127,6 +137,25 @@ const Bootcamps = () => {
                                 <p><strong>Early Bird Price:</strong> ₦170,000 (Deadline: Jan 24, 2025)</p>
                             </div>
                         </div>
+                    </SectionTitle.Description>
+
+                    <SectionTitle.Description>
+                        <SectionTitle.Title>Frequently Asked Questions (FAQ):</SectionTitle.Title>
+                        <ul>
+                            <li><strong>Q: How long is the Bootcamp?</strong> <br />A: The Bootcamp is a 3-month program.</li>
+                            <li><strong>Q: Do I need prior tech experience?</strong> <br />A: No, our Bootcamp is beginner-friendly and designed for individuals with no prior experience.</li>
+                            <li><strong>Q: Can I get a job after completing the Bootcamp?</strong> <br />A: Yes, we provide job placement assistance and virtual internship opportunities to help you land your first role.</li>
+                        </ul>
+                     </SectionTitle.Description>
+
+                     <SectionTitle.Description>
+                    <SectionTitle.Title>Register Now &amp; Secure Your Spot</SectionTitle.Title>
+                        <p>Ready to take the next step? Pay to the account details provided, and click I have paid button to share your proof of payment with us.</p>
+                        <p><strong>Account Name:</strong> Afribreath- Afriheals Car-lco <br />
+                        <strong>Account Number:</strong> 9627497057 <br />
+                        <strong>Bank:</strong> Providus Bank
+                        </p>
+
                     </SectionTitle.Description>
 
                     <SectionTitle.Description>
