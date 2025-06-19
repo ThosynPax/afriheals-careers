1526746060ea11591ccca1107c886cd57a469d95
Thosyn Pax <thosynpax@gmail.com>
2025-06-19 19:30:17 +0100

    updated error

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,7 @@ import Hero from '@components/sections/hero'
 import About from '@components/sections/about'
 import Testimonials from '@components/sections/testimonials'
 import Booking from '@components/sections/booking'
-import Terms from '@components/others/Terms'
-import Privacy from '@components/others/Privacy'
-import Courses from '@components/others/Courses'
 import Services from '@components/sections/services'
-import Bootcamps from '@components/others/Bootcamps'
 import Story from '@components/sections/Story'
 import Projects from '@components/sections/projects'
 import Pricing from '@components/sections/pricing'
@@ -20,50 +16,12 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        {/* Route for Terms Page */}
-        <Route path="/terms" element={
-          <>
-            <Header />
-            <Terms />
-            <Footer />
-            <ScrollToTop />
-          </>
-        } />
-
-        {/* Route for Privacy Page */}
-        <Route path="/privacy" element={
-          <>
-            <Header />
-            <Privacy />
-            <Footer />
-            <ScrollToTop />
-          </>
-        } />
-
-         {/* Route for Courses Page */}
-         <Route path="/courses" element={
-          <>
-            <Header />
-            <Courses />
-            <Footer />
-            <ScrollToTop />
-          </>
-        } />
 
         {/* Route for Story Page */}
         <Route path="/story" element={
           <>
             <Header />
             <Story />
-            <Footer />
-            <ScrollToTop />
-          </>
-        } />
-
-        {/* Route for Bootcamps Page */}
-        <Route path="/Bootcamps" element={
-          <>
-            <Bootcamps />
             <Footer />
             <ScrollToTop />
           </>
